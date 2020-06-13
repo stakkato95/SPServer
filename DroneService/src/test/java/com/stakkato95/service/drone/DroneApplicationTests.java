911ff5b840ec20172df7cbe1a11b2b760ae9f3dc
Artsiom Kaliaha <stakkato95@gmail.com>
2020-06-13 10:37:46 +0200

    Add drone service

---
+++ DroneService/src/test/java/com/stakkato95/service/drone/DroneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stakkato95.service.drone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
